07c5a90bfad1f44dd0aab2ca2196f1f3e6b6f90b
darkMode <iransoftnet.ir@gmail.com>
2021-10-11 08:27:36 +0330

    feat: use strategy msg sender

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,9 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+$sender = new \App\MsgSender(app());
+$sender->send('09120000231','Hi This is the test text');
+
+dd('');
