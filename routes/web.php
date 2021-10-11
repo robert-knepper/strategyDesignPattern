523c9dc415873f0cee1460844b6f1c63654af931
darkMode <iransoftnet.ir@gmail.com>
2021-10-11 08:32:05 +0330

    feat: call msg sender for ioc bind

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,8 @@
 
 
 
-$sender = new \App\MsgSender(app());
+//$sender = new \App\MsgSender(app());
+$sender = app('\App\MsgSender');
 $sender->send('09120000231','Hi This is the test text');
 
 dd('');
